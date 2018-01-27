5fe91501a33c3dd4b6af91b068842f5c3c4c5a31
June Cho <junecho.ucb@gmail.com>
2018-01-27 13:06:41 -0800

    Added Slider

--- remix/Layouts/Stack.xaml.cs
+++ remix/Layouts/Stack.xaml.cs
@@ -10,6 +10,13 @@ namespace remix.Layouts
         public Stack()
         {
             InitializeComponent();
+
+            slider.Value = 0.0;
+        }
+
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            label.Text = String.Format("Value is {0:F2}", e.NewValue);
         }
     }
 }
