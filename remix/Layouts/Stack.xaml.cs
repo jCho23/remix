5d5d68b205cf5aecdd40a6af102d550d6852df57
June Cho <junecho.ucb@gmail.com>
2018-01-27 16:45:14 -0800

    Added Bindings

--- remix/Layouts/Stack.xaml.cs
+++ remix/Layouts/Stack.xaml.cs
@@ -14,9 +14,9 @@ namespace remix.Layouts
             slider.Value = 0.0;
         }
 
-        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
-        {
-            label.Text = String.Format("Value is {0:F2}", e.NewValue);
-        }
-    }
+    //    void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+    //    {
+    //        label.Text = String.Format("Value is {0:F2}", e.NewValue);
+    //    }
+     }
 }
