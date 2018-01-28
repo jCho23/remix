f447e6141b6485724bab41e364f8dfccfce8fa12
June Cho <junecho.ucb@gmail.com>
2018-01-28 08:46:54 -0800

    CleanUp

---
+++ remix/Layouts/sanity.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace remix.Layouts
+{
+    public partial class sanity : ContentPage
+    {
+        public sanity()
+        {
+            InitializeComponent();
+
+            slider1.Value = 0.5;
+        }
+    }
+}
